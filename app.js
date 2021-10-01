25c447795019e8a21b6e071827309b0d3af13894
Sr7-Rastogi <shubhrastogi777@gmail.com>
2021-10-01 19:27:35 +0530

    array method

--- app.js
+++ app.js
@@ -1,37 +1,7 @@
-const a = document.querySelectorAll('input')[0]
-const b = document.querySelectorAll('input')[1]
-const add = document.querySelector('#add')
-const multiply = document.querySelector("#multiply")
-const sub = document.querySelector("#sub")
-const result = document.querySelector('.result')
-    // const sum = () => {
-    //     const ans = parseInt(a.value) + parseInt(b.value);
-    //     result.innerHTML = ans;
-    // }
-    // const product = () => {
-    //     const ans = parseInt(a.value) * parseInt(b.value);
-    //     result.innerHTML = ans;
-    // }
-const calculator = (event, operation) => {
-    console.log(event.target)
-
-    switch (operation) {
-        case "add":
-            result.innerHTML = parseInt(a.value) + parseInt(b.value);
-            break;
-
-        case "multiply":
-            result.innerHTML = parseInt(a.value) * parseInt(b.value);
-            break;
-        case "sub":
-            result.innerHTML = parseInt(a.value) - parseInt(b.value);
-            break;
-        default:
-            result.innerHTML = "Not Valid Operation"
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var ar2 = arr.filter((element, index) => {
+    if (element % 2 === 0) {
+        return true;
     }
-
-}
-
-add.addEventListener('click', (event) => calculator(event, 'add'))
-multiply.addEventListener('click', (event) => calculator(event, 'multiply'))
-sub.addEventListener('click', (event) => calculator(event, 'sub'))
+})
+console.log(ar2)
