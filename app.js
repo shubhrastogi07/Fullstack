aa55c4ab655703f965edbf3845b6b652efafc108
Sr7-Rastogi <shubhrastogi777@gmail.com>
2021-10-06 19:11:18 +0530

    fetching data from api and arranging it in a table

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
-var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
+// let value = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("Shubh Rastogi");
+//         }, 3000);
+//     }).then(value => console.log(value))
+//     // console.log(value)
+//     // setTimeout(() => {
+//     //     console.log(value)
+//     // }, 4000)
+const table = document.querySelector('table')
+console.log(table)
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(user => {
+            let newRow = document.createElement('tr')
+            let element = ` 
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.username}</td>`
+            newRow.innerHTML = element
+            table.appendChild(newRow)
 
-var chores = []
+        })
 
-var deleteItem = (value) => {
-    console.log(value)
-    const index = chores.indexOf(value)
-    chores.splice(index, 1);
-    console.log(chores)
-
-}
-
-
-const callbackfunc = (event) => {
-    // console.log(input.value)
-    const inputValue = input.value
-    if (chores.includes(inputValue)) {
-        console.log('already exists')
-    } else {
-        chores.push(inputValue)
-        const element = document.createElement('li')
-        const textNode = document.createTextNode(inputValue)
-        element.appendChild(textNode)
-        list.appendChild(element)
-        element.addEventListener('click', (e) => { deleteItem(e.target.innerHTML) })
-    }
-}
-
-
-button.addEventListener('click', callbackfunc)
+    })
