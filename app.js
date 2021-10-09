a42d3ea9bf6e511e3f82533ab07c89a339442588
Sr7-Rastogi <shubhrastogi777@gmail.com>
2021-10-09 18:08:19 +0530

    recipe app

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-// let value = new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve("Shubh Rastogi");
-//         }, 3000);
-//     }).then(value => console.log(value))
-//     // console.log(value)
-//     // setTimeout(() => {
-//     //     console.log(value)
-//     // }, 4000)
-const table = document.querySelector('table')
-console.log(table)
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        data.forEach(user => {
-            let newRow = document.createElement('tr')
-            let element = ` 
-            <td>${user.id}</td>
-            <td>${user.name}</td>
-            <td>${user.email}</td>
-            <td>${user.username}</td>`
-            newRow.innerHTML = element
-            table.appendChild(newRow)
+const but = document.querySelector('button')
+const getText = document.querySelector('input')
+const parent = document.querySelector('div.grid')
 
-        })
-
+const getData = async(item) => {
+    console.log(item)
+    const url = `https://api.edamam.com/search?app_id=e7c6fc27&app_key=58511197453dd68a356ab410c1b1c877&q=${item}`
+    console.log(url)
+    const res = await fetch(url)
+    const data = await res.json()
+    const recipies = data.hits
+    recipies.forEach(item => {
+        const { recipe } = item
+        console.log(recipe)
+        const { label, image, source, calories } = recipe
+        const template = ` 
+                        <img class="card__image" src="${image}" />
+                        <div class="card__data">
+                            <div class="card__info">
+                                <h2>${label}</h2>
+                                <p>From ${source}</p>
+                            </div>
+                            <h3 class="card__price">${calories.toFixed(2)}</h3>
+                            <button class="card__add">+</button>
+                        </div>
+                    `
+        const newCard = document.createElement('article')
+        newCard.setAttribute('class', 'card')
+        newCard.setAttribute('style', 'margin-bottom:50px')
+        newCard.innerHTML = template
+        console.log(newCard)
+        parent.appendChild(newCard)
     })
+
+}
+
+//  add event listener to the button
+but.addEventListener('click', (e) => {
+    getData(getText.value)
+})
