d24807072c937d6fa85f8ba3724c5a4d60320819
Sr7-Rastogi <shubhrastogi777@gmail.com>
2021-10-16 18:25:57 +0530

    middleware & authetication

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const express = require('express')
 const PORT = 3000
 const app = express()
-    // const verify = (req, res, next) => {
-    //     console.log(req.headers['user-agent'])
-    //     if (req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io)") next()
-    //     else res.send('Blocked')
-    // }
+    //     // const verify = (req, res, next) => {
+    //     //     console.log(req.headers['user-agent'])
+    //     //     if (req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io)") next()
+    //     //     else res.send('Blocked')
+    //     // }
 
-const isAdmin = (req, res, next) => {
-    if (req.headers.admin === 'true') next()
-    else res.send("UNAUTHORISED")
-}
+// const isAdmin = (req, res, next) => {
+//     if (req.headers.admin === 'true') next()
+//     else res.send("UNAUTHORISED")
+// }
 
 
-app.get('/public', (req, res) => {
-    console.log(req.headers)
-    res.send(`I'm a public route`)
-})
+// app.get('/public', (req, res) => {
+//     console.log(req.headers)
+//     res.send(`I'm a public route`)
+// })
 
-app.get('/private', isAdmin, (req, res) => {
-    res.send(`I'm a admin route`)
+// app.get('/private', isAdmin, (req, res) => {
+//     res.send(`I'm a admin route`)
+// })
+app.use(express.json())
+app.post('/signup', (req, res) => {
+    console.log(req.body)
+    res.send('dadada')
 })
 
 app.listen(PORT, () => {
